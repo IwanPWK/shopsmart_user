2328be9c0cfb51be35fced157704555602a9f1fc
IwanPWK <koadhyow@gmail.com>
2023-12-23 10:03:08 +0700

    Create text widget

--- lib/consts/theme_data.dart
+++ lib/consts/theme_data.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'app_colors.dart';
 
+// use BuildContext if  want specific
 class Styles {
   static ThemeData themeData({required bool isDarkTheme}) {
     return ThemeData(
@@ -11,6 +12,7 @@ class Styles {
         cardColor: isDarkTheme
             ? const Color.fromARGB(255, 13, 6, 37)
             : AppColors.lightCardColor,
-        brightness: isDarkTheme ? Brightness.dark : Brightness.light);
+        brightness:
+            isDarkTheme ? Brightness.dark : Brightness.light); // for text
   }
 }
